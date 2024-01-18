2f8163c2d917d5ee5cd9c2febc8994960b891278
Vadim Batyashov <changcheng39@gmail.com>
2024-01-18 17:28:56 +0200

    Add simple singleton

---
+++ Patterns/Singleton/SimpleSingleton.java
@@ -0,0 +1,11 @@
+final class SimpleSingleton {
+    private static Singleton instance = null;
+
+    private Singleton() {}
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+}
