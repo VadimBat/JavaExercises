7ac6cdd8ac5f50f5321ad051d0e8e13c17089a00
Vadim Batyashov <changcheng39@gmail.com>
2024-01-26 18:15:11 +0200

    Fix singleton

---
+++ Patterns/Creational/Singleton/DemoSingleton.java
@@ -0,0 +1,16 @@
+public class DemoSingleton {
+    public static void main(String[] args) {
+        System.out.println(
+                new StringBuilder()
+                        .append("If you see the same value, then singleton was reused")
+                        .append("\n")
+                        .append("If you see different values, then 2 singletons were created")
+                        .append("\n\n")
+                        .append("RESULT:")
+                        .append("\n").toString());
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
