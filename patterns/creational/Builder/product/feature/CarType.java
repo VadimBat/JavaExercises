085b6f89e5a08a63f3b9d983cce46ee733dbb859
Vadim Batyashov <changcheng39@gmail.com>
2024-02-01 02:19:16 +0200

    Add builder pattern

---
+++ patterns/creational/Builder/product/feature/CarType.java
@@ -0,0 +1,5 @@
+package product.feature;
+
+public enum CarType {
+    CITY_CAR, SPORTS_CAR, SUV
+}
---
+++ patterns/creational/Builder/product/feature/Transmission.java
@@ -0,0 +1,5 @@
+package product.feature;
+
+public enum Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
+}
